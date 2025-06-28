d50bc16729b2bbe5d9e04507ca48beb2349ea29d
storeps <egipr0606@gmail.com>
2025-06-28 08:42:57 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100 p-6">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-3xl font-bold mb-4 text-center">STOREPS (PRIVATE SERVER)</h1>
+        <p className="text-lg text-center">Selamat datang di Dashboard STOREPS.</p>
+      </div>
+    </main>
+  );
+}
